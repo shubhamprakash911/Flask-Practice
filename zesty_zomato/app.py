f784e36338866927a5ba2b0e651736133485318b
shubhamprakash911 <shubhamprakash9112000@gmail.com>
2023-07-06 11:25:24 +0530

    version 2

--- zesty_zomato/app.py
+++ zesty_zomato/app.py
@@ -1,33 +1,94 @@
 from flask import Flask, request, jsonify,render_template
-import json
+# import json
 from pymongo import MongoClient
+# from bson.objectid import ObjectId
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
 app = Flask(__name__)
 
-# client = MongoClient("mongodb+srv://shubham:shubham@cluster0.lsxduhy.mongodb.net/flaskDB")
-# db = client['flaskDB']
 
-# Load menu data from JSON file
+client = MongoClient("mongodb+srv://shubham:shubham@cluster0.lsxduhy.mongodb.net/flaskDB?retryWrites=true&w=majority")
+db = client['flaskDB']
+menu_collection = db["menu"]
+orders_collection = db["orders"]
+
+
+
+# # Load menu data from JSON file
+# def load_menu():
+#     with open('menu.json', 'r') as file:
+#         return json.load(file)
+
+# # Save menu data to JSON file
+# def save_menu(menu):
+#     with open('menu.json', 'w') as file:
+#         json.dump(menu, file,indent=4)
+
+# # Load order data from JSON file
+# def load_orders():
+#     with open('orders.json', 'r') as file:
+#         return json.load(file)
+
+# # Save order data to JSON file
+# def save_orders(orders):
+#     with open('orders.json', 'w') as file:
+#         json.dump(orders, file,indent=4)
+
+
+
+
+# Load menu data from MongoDB collection
 def load_menu():
-    with open('menu.json', 'r') as file:
-        return json.load(file)
+    return list(menu_collection.find({},{'_id':0}))
 
-# Save menu data to JSON file
+# Save menu data to MongoDB collection
 def save_menu(menu):
-    with open('menu.json', 'w') as file:
-        json.dump(menu, file,indent=4)
+    menu_collection.delete_many({})  # Clear existing data
+    menu_collection.insert_many(menu)
 
-# Load order data from JSON file
+# Load order data from MongoDB collection
 def load_orders():
-    with open('orders.json', 'r') as file:
-        return json.load(file)
+    return list(orders_collection.find({},{'_id':0}))
 
-# Save order data to JSON file
+# Save order data to MongoDB collection
 def save_orders(orders):
-    with open('orders.json', 'w') as file:
-        json.dump(orders, file,indent=4)
+    orders_collection.delete_many({})  # Clear existing data
+    orders_collection.insert_many(orders)
 
+
+
+# chatbot code
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    return get_Chat_response(input)
+
+
+def get_Chat_response(text):
+
+    # Let's chat for 5 lines
+    for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+
+        # append the new user input tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+        # generated a response while limiting the total chat history to 1000 tokens, 
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+        # pretty print last ouput tokens from bot
+        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    
+
+# chatbot code end
 
 @app.route('/')
 def index():
